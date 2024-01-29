34b427261b5650c6a5d04b370de8503c534bee26
taichiii <aogaku415@gmail.com>
2024-01-30 06:13:06 +0900

    購入成功ページのpathが間違っていたので修正

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -25,7 +25,7 @@ export async function POST(request: Request, response: Response) {
         },
       ],
       mode: 'payment',
-      success_url: `${process.env.NEXT_PUBLIC_BASE_URL}book/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
     })
     return NextResponse.json({ checkout_url: session.url })
