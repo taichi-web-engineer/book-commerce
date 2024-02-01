a94401cd3bc069f422f3aa88414531c2518c5d64
taichiii <aogaku415@gmail.com>
2024-02-01 11:20:24 +0900

    Book.tsxでuserインスタンスを作成せずに呼び出し元で渡す

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,6 +37,7 @@ export default async function Home() {
             key={book.id}
             book={book}
             isPurchased={purchaseBookIds.includes(book.id)}
+            user={user}
           />
         ))}
       </main>
